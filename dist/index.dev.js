807c2e621a8f0e2a3c95051971382731843e129d
Hien Nguyen <nguyenconghien09@gmail.com>
2020-06-02 16:55:52 +0700

    first commit

---
+++ dist/index.dev.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+var port = 3000;
+var users = [{
+  id: 1,
+  name: 'Linh'
+}, {
+  id: 2,
+  name: 'Hiii'
+}];
+app.set('view engine', 'pug');
+app.set('views', './views');
+app.get('/users', function (req, res) {
+  res.render('users/index', {
+    users: users
+  });
+});
+app.get('/', function (req, res) {
+  res.render('index', {
+    name: 'Linhhh'
+  });
+});
+app.get('/users/search', function (req, res) {
+  var q = req.query.q;
+  var matchUsers = users.filter(function (user) {
+    return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+  });
+  res.render('users/index', {
+    users: matchUsers
+  });
+});
+app.listen(port, function () {
+  console.log('Server learning on port' + port);
+});
